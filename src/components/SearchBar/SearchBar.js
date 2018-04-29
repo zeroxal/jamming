618ed1b1b21686640fead0a8491799a370446727
Josef Chen <zeroxal10@gmail.com>
2018-04-29 23:50:59 +0200

    Add Enter feature

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,29 +1,32 @@
-
 import React from 'react';
 import './Searchbar.css'
 
-export default class SearchBar extends React.Component{
-constructor(props){
-  super(props);
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  this.search = this.search.bind(this);
-  this.handleEvent = this.handleEvent.bind(this);
-}
-
-
-handleEvent(event) {
-  this.setState({search: event.target.value})
-}
-
-search() {
-  this.props.onSearch(this.state.search);
-}
-
-
-  render() {  return (
-      <div className="SearchBar">
-    <input onChange={this.handleEvent} placeholder="Enter A Song, Album, or Artist" />
-  <a onClick={this.search}>SEARCH</a>
-  </div>
-    );}
+    this.search = this.search.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this)
   }
+
+  handleEvent(event) {
+    this.setState({search: event.target.value})
+  }
+
+  search() {
+    this.props.onSearch(this.state.search);
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.props.onSearch(this.state.search);
+    }
+  }
+  render() {
+    return (<div className="SearchBar">
+      <input onChange={this.handleEvent} placeholder="Enter A Song, Album, or Artist" onKeyPress={this.handleKeyPress}/>
+      <a onClick={this.search}>SEARCH</a>
+    </div>);
+  }
+}
